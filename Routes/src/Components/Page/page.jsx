5ca32e0f0d23539e3b2b54eeed87edbdab0aa417
Alexander-D-28 <justalexander28@gmail.com>
2026-09-19 14:12:20 -0400

    Page setup and country input

--- Routes/src/Components/Page/page.jsx
+++ Routes/src/Components/Page/page.jsx
@@ -1,8 +1,36 @@
+import './page.css'
+import countries from './images/countries.png'
+
 function HomePage() {
     return (
-        <main>
-            <p>hi</p>
-        </main>
+        <>
+            <header className="header">
+                <h3>C.H. Robinson Take Home Project</h3>
+                <h4>- By Alexander Dobek</h4>
+            </header>
+            <hr />
+            <main>
+                <div className="content">
+                    <img src={countries} alt="image of North American Countries" />
+                    <div className="input">
+                        <p>Source: United States</p>
+                        <select>
+                            <option value="" disabled selected>Select a Destination</option>
+                            <option value="Canada">Canada</option>
+                            <option value="Mexico">Mexico</option>
+                            <option value="Belize">Belize</option>
+                            <option value="Guatemala">Guatemala</option>
+                            <option value="El Salvador">El Salvador</option>
+                            <option value="Honduras">Honduras</option>
+                            <option value="Nicaragua">Nicaragua</option>
+                            <option value="Costa Rica">Costa Rica</option>
+                            <option value="Panama">Panama</option>
+                        </select>
+                    </div>
+                </div>
+            </main>
+        </>
     )
 }
+
 export default HomePage
