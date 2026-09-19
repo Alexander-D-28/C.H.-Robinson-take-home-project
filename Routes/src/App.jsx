2802c40c41ce10339f370995e8d78cfad5627ae4
Alexander-D-28 <justalexander28@gmail.com>
2026-09-19 13:40:00 -0400

    Initial Project setup

---
+++ Routes/src/App.jsx
@@ -0,0 +1,7 @@
+import HomePage from './Components/Page/page'
+
+function App() {
+  return <HomePage />
+}
+
+export default App
---
+++ Routes/src/Components/Page/page.jsx
@@ -0,0 +1,8 @@
+function HomePage() {
+    return (
+        <main>
+            <p>hi</p>
+        </main>
+    )
+}
+export default HomePage
